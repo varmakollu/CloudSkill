0d547813657a29fb88687f9f2a7c987221e49737
Varma Kollu <varmaprince24@yahoo.com>
2024-04-29 15:11:26 +0530

    Update quicklabarc122.sh

--- Challenge Labs/Analyze Images with the Cloud Vision API: Challenge Lab/quicklabarc122.sh
+++ Challenge Labs/Analyze Images with the Cloud Vision API: Challenge Lab/quicklabarc122.sh
@@ -1,6 +1,12 @@
 
 
 
+gcloud config set compute/region $REGION
+
+
+gsutil acl ch -u AllUsers:R gs://$DEVSHELL_PROJECT_ID-bucket/manif-des-sans-papiers.jpg
+
+sleep 20
 
 cat > request.json <<EOF
 {
@@ -54,10 +60,6 @@ EOF
 curl -s -X POST -H "Content-Type: application/json" --data-binary @request.json  https://vision.googleapis.com/v1/images:annotate?key=${API_KEY} -o landmark-response.json
 
 gsutil cp landmark-response.json gs://$DEVSHELL_PROJECT_ID-bucket
-
-
-
-
 
 
 
