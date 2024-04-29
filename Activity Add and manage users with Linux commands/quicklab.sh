f991dacb0ad4d1a63f45661d4a0f27e24f64a0f6
Varma Kollu <varmaprince24@yahoo.com>
2024-04-30 02:04:57 +0530

    Create quicklab.sh

---
+++ Activity Add and manage users with Linux commands/quicklab.sh
@@ -0,0 +1,14 @@
+
+
+
+
+
+sudo useradd researcher9
+
+sudo usermod -aG research_team researcher9
+
+sudo chown researcher9:research_team /home/researcher2/projects/project_r.txt
+
+sudo usermod -aG sales_team researcher9
+
+
