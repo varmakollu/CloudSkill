fa7fe10676d43982e9a4af05f1ef5d2d5edb6e2b
Varma Kollu <varmaprince24@yahoo.com>
2024-04-29 22:28:02 +0530

    Update quicklabgsp281.sh

--- Introduction to SQL for BigQuery and Cloud SQL/quicklabgsp281.sh
+++ Introduction to SQL for BigQuery and Cloud SQL/quicklabgsp281.sh
@@ -3,8 +3,8 @@
 gsutil mb gs://$DEVSHELL_PROJECT_ID
 
 
-curl -O https://raw.githubusercontent.com/quiccklabs/Labs_solutions/master/Introduction%20to%20SQL%20for%20BigQuery%20and%20Cloud%20SQL/start_station_name.csv
-curl -O https://raw.githubusercontent.com/quiccklabs/Labs_solutions/master/Introduction%20to%20SQL%20for%20BigQuery%20and%20Cloud%20SQL/end_station_name.csv
+curl -O https://raw.githubusercontent.com/varmakollu/CloudSkill/main/Introduction%20to%20SQL%20for%20BigQuery%20and%20Cloud%20SQL/start_station_name.csv
+curl -O https://raw.githubusercontent.com/varmakollu/CloudSkill/main/Introduction%20to%20SQL%20for%20BigQuery%20and%20Cloud%20SQL/end_station_name.csv
 
 gsutil cp start_station_name.csv gs://$DEVSHELL_PROJECT_ID/
 gsutil cp end_station_name.csv gs://$DEVSHELL_PROJECT_ID/
