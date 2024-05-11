23b7b4806e612b8020a43704cba52f0c5a903233
Varma Kollu <varmaprince24@yahoo.com>
2024-05-11 09:45:10 +0530

    Update quicklabgsp903.sh

--- Stream Processing with Cloud Pub:Sub and Dataflow Qwik Start/quicklabgsp903.sh
+++ Stream Processing with Cloud Pub:Sub and Dataflow Qwik Start/quicklabgsp903.sh
@@ -19,6 +19,8 @@ gcloud pubsub topics create $TOPIC_ID
 
 if [ "$REGION" == "us-central1" ]; then
   gcloud app create --region us-central
+elif [ "$REGION" == "europe-west1" ]; then
+  gcloud app create --region europe-west
 else
   gcloud app create --region "$REGION"
 fi
@@ -26,25 +28,32 @@ fi
 gcloud scheduler jobs create pubsub publisher-job --schedule="* * * * *" \
     --topic=$TOPIC_ID --message-body="Hello!"
 
-gcloud scheduler jobs run publisher-job
+sleep 60
 
-git clone https://github.com/GoogleCloudPlatform/java-docs-samples.git
-cd java-docs-samples/pubsub/streaming-analytics
+gcloud scheduler jobs run publisher-job --location=$REGION
 
-mvn compile exec:java \
--Dexec.mainClass=com.examples.pubsub.streaming.PubSubToGcs \
--Dexec.cleanupDaemonThreads=false \
--Dexec.args=" \
-    --project=$PROJECT_ID \
-    --region=$REGION \
-    --inputTopic=projects/$PROJECT_ID/topics/$TOPIC_ID \
-    --output=gs://$BUCKET_NAME/samples/output \
-    --runner=DataflowRunner \
-    --windowSize=2"
+sleep 60
+
+gcloud scheduler jobs run publisher-job --location=$REGION
+
+cat > automate_commands.sh <<EOF_END
+#!/bin/bash
+
+git clone https://github.com/GoogleCloudPlatform/python-docs-samples.git
+cd python-docs-samples/pubsub/streaming-analytics
+pip install -U -r requirements.txt
+python PubSubToGCS.py \
+--project=$PROJECT_ID \
+--region=$REGION \
+--input_topic=projects/$PROJECT_ID/topics/$TOPIC_ID \
+--output_path=gs://$BUCKET_NAME/samples/output \
+--runner=DataflowRunner \
+--window_size=2 \
+--num_shards=2 \
+--temp_location=gs://$BUCKET_NAME/temp
+EOF_END
+
+chmod +x automate_commands.sh
 
 
-
-
-
-
-
+docker run -it -e DEVSHELL_PROJECT_ID=$DEVSHELL_PROJECT_ID -e BUCKET_NAME=$BUCKET_NAME -e PROJECT_ID=$PROJECT_ID -e REGION=$REGION -e TOPIC_ID=$TOPIC_ID -v $(pwd)/automate_commands.sh:/automate_commands.sh python:3.7 /bin/bash -c "/automate_commands.sh"
