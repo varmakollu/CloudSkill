ce19f2b93ce7b57110acac29d4a5135acb3577e7
Varma Kollu <varmaprince24@yahoo.com>
2024-12-28 10:26:11 +0000

    CloudSkill

---
+++ API Gateway: Qwik Start/quicklab.md
@@ -0,0 +1,18 @@
+# API Gateway: Qwik Start || [GSP872](https://www.cloudskillsboost.google/focuses/17996?parent=catalog) ||
+
+## 💡 Solution [here](https://youtu.be/UamVaw9hPdE)
+
+### Run the following Commands in CloudShell
+
+```
+export REGION=
+```
+```
+curl -LO raw.githubusercontent.com/varmakollu/CloudSkill/refs/heads/main/API%20Gateway%3A%20Qwik%20Start/quicklabgcp872.sh
+
+sudo chmod +x quicklabgcp872.sh
+
+./quicklabgcp872.sh
+```
+
+### Congratulations 🎉 for completing the Lab !
--- API Gateway: Qwik Start/quicklabgcp872.sh
+++ API Gateway: Qwik Start/quicklabgcp872.sh
@@ -1,54 +1,81 @@
+#!/bin/bash
+# Define color variables
 
 
-gcloud auth list
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${BG_MAGENTA}${BOLD}Starting Execution${RESET}"
 
 export PROJECT_ID=$(gcloud config get-value project)
 
 gcloud config set compute/region $REGION
 
 gcloud services enable apigateway.googleapis.com --project $DEVSHELL_PROJECT_ID
+gcloud services enable run.googleapis.com
 
 sleep 15
 
 export PROJECT_NUMBER=$(gcloud projects describe $PROJECT_ID --format="value(projectNumber)")
 
-
 gcloud projects add-iam-policy-binding $PROJECT_ID --member="serviceAccount:$PROJECT_NUMBER-compute@developer.gserviceaccount.com" --role="roles/serviceusage.serviceUsageAdmin"
 
 gcloud projects add-iam-policy-binding $PROJECT_ID --member="serviceAccount:$PROJECT_NUMBER-compute@developer.gserviceaccount.com" --role="roles/artifactregistry.reader"
 
-
+sleep 30
 
 git clone https://github.com/GoogleCloudPlatform/nodejs-docs-samples.git
 
 cd nodejs-docs-samples/functions/helloworld/helloworldGet
 
-
-#!/bin/bash
+sleep 60
 
 deploy_function() {
-  gcloud functions deploy helloGET --runtime nodejs14 --trigger-http --allow-unauthenticated --region $REGION
+  gcloud functions deploy helloGET \
+    --runtime nodejs20 \
+    --region $REGION \
+    --trigger-http \
+    --allow-unauthenticated
 }
 
 deploy_success=false
 
 while [ "$deploy_success" = false ]; do
   if deploy_function; then
-    echo "Function deployed successfully."
+    echo "Cloud Run service is created. Exiting the loop."
     deploy_success=true
   else
-    echo "Retrying, please subscribe to tutorialboy (https://www.youtube.com/@tutorialboy24)..."
-    sleep 30
+    echo "Waiting for Cloud Run service to be created..."
+    sleep 60
   fi
 done
 
+echo "Running the next code..."
 
 gcloud functions describe helloGET --region $REGION
 
 curl -v https://$REGION-$PROJECT_ID.cloudfunctions.net/helloGET
 
 cd ~
-
 
 cat > openapi2-functions.yaml <<EOF_CP
 # openapi2-functions.yaml
@@ -67,7 +94,7 @@ paths:
       summary: Greet a user
       operationId: hello
       x-google-backend:
-        address: https://$REGION-$PROJECT_ID.cloudfunctions.net/helloGET
+        address: https://us-east4-qwiklabs-gcp-01-b47a65687b9f.cloudfunctions.net/helloGET
       responses:
        '200':
           description: A successful response
@@ -75,19 +102,13 @@ paths:
             type: string
 EOF_CP
 
-
-
 export API_ID="hello-world-$(cat /dev/urandom | tr -dc 'a-z' | fold -w ${1:-8} | head -n 1)"
-
 
 sed -i "s/API_ID/${API_ID}/g" openapi2-functions.yaml
 sed -i "s/PROJECT_ID/$PROJECT_ID/g" openapi2-functions.yaml
 
-
 export API_ID="hello-world-$(cat /dev/urandom | tr -dc 'a-z' | fold -w ${1:-8} | head -n 1)"
 echo $API_ID
-
-
 
 gcloud api-gateway apis create "hello-world-api"  --project=$PROJECT_ID
 
@@ -95,11 +116,9 @@ gcloud api-gateway api-configs create hello-world-config --project=$PROJECT_ID -
 
 gcloud api-gateway gateways create hello-gateway --location=$REGION --project=$PROJECT_ID --api=$API_ID --api-config=hello-world-config
 
+gcloud alpha services api-keys create --display-name="awesome"  
 
-
-gcloud alpha services api-keys create --display-name="tutorialboy"  
-
-KEY_NAME=$(gcloud alpha services api-keys list --format="value(name)" --filter "displayName=tutorialboy") 
+KEY_NAME=$(gcloud alpha services api-keys list --format="value(name)" --filter "displayName=awesome") 
 
 export API_KEY=$(gcloud alpha services api-keys get-key-string $KEY_NAME --format="value(keyString)") 
 
@@ -108,9 +127,7 @@ echo $API_KEY
 MANAGED_SERVICE=$(gcloud api-gateway apis list --format json | jq -r .[0].managedService | cut -d'/' -f6)
 echo $MANAGED_SERVICE
 
-
 gcloud services enable $MANAGED_SERVICE
-
 
 cat > openapi2-functions2.yaml <<EOF_CP
 # openapi2-functions.yaml
@@ -148,16 +165,11 @@ EOF_CP
 sed -i "s/API_ID/${API_ID}/g" openapi2-functions2.yaml
 sed -i "s/PROJECT_ID/$PROJECT_ID/g" openapi2-functions2.yaml
 
-
-
 gcloud api-gateway api-configs create hello-config --project=$PROJECT_ID \
   --display-name="Hello Config" --api=$API_ID --openapi-spec=openapi2-functions2.yaml \
   --backend-auth-service-account=$PROJECT_ID@$PROJECT_ID.iam.gserviceaccount.com	
 
-
-
 gcloud api-gateway gateways update hello-gateway --location=$REGION --project=$PROJECT_ID --api=$API_ID --api-config=hello-config
-
 
 gcloud projects add-iam-policy-binding $PROJECT_ID --member="serviceAccount:$PROJECT_ID@$PROJECT_ID.iam.gserviceaccount.com" --role="roles/serviceusage.serviceUsageAdmin"
 
@@ -175,4 +187,6 @@ curl -sL $GATEWAY_URL/hello
 
 curl -sL -w "\n" $GATEWAY_URL/hello?key=$API_KEY
 
+echo "${BG_RED}${BOLD}Congratulations For Completing The Lab - TutorialBoy !!!${RESET}"
 
+#-----------------------------------------------------end----------------------------------------------------------#
