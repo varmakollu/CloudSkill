66a965e487332bacd91c23a8164e888e46fd36e5
Varma Kollu <varmaprince24@yahoo.com>
2025-01-09 10:26:59 +0530

    Cloud Skill Boost

--- Analyze Images with the Cloud Vision API Challenge Lab/quicklabarc122.sh
+++ Analyze Images with the Cloud Vision API Challenge Lab/quicklabarc122.sh
@@ -1,12 +1,39 @@
+#!/bin/bash
+# Define color variables
 
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
 
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
 
-gcloud config set compute/region $REGION
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
 
+echo "${BG_MAGENTA}${BOLD}Starting Execution${RESET}"
 
-gsutil acl ch -u AllUsers:R gs://$DEVSHELL_PROJECT_ID-bucket/manif-des-sans-papiers.jpg
+gcloud alpha services api-keys create --display-name="awesome"
 
-sleep 20
+KEY_NAME=$(gcloud alpha services api-keys list --format="value(name)" --filter "displayName=awesome")
+
+export API_KEY=$(gcloud alpha services api-keys get-key-string $KEY_NAME --format="value(keyString)")
+
+export PROJECT_ID=$(gcloud config list --format 'value(core.project)')
+
+gsutil acl ch -u allUsers:R gs://$PROJECT_ID-bucket/manif-des-sans-papiers.jpg
 
 cat > request.json <<EOF
 {
@@ -14,7 +41,7 @@ cat > request.json <<EOF
       {
         "image": {
           "source": {
-              "gcsImageUri": "gs://$DEVSHELL_PROJECT_ID-bucket/manif-des-sans-papiers.jpg"
+              "gcsImageUri": "gs://$PROJECT_ID-bucket/manif-des-sans-papiers.jpg"
           }
         },
         "features": [
@@ -28,13 +55,11 @@ cat > request.json <<EOF
 }
 EOF
 
-
 curl -s -X POST -H "Content-Type: application/json" --data-binary @request.json  https://vision.googleapis.com/v1/images:annotate?key=${API_KEY}
 
 curl -s -X POST -H "Content-Type: application/json" --data-binary @request.json  https://vision.googleapis.com/v1/images:annotate?key=${API_KEY} -o text-response.json
 
-gsutil cp text-response.json gs://$DEVSHELL_PROJECT_ID-bucket
-
+gsutil cp text-response.json gs://$PROJECT_ID-bucket
 
 cat > request.json <<EOF
 {
@@ -42,7 +67,7 @@ cat > request.json <<EOF
       {
         "image": {
           "source": {
-              "gcsImageUri": "gs://$DEVSHELL_PROJECT_ID-bucket/manif-des-sans-papiers.jpg"
+              "gcsImageUri": "gs://$PROJECT_ID-bucket/manif-des-sans-papiers.jpg"
           }
         },
         "features": [
@@ -56,10 +81,10 @@ cat > request.json <<EOF
 }
 EOF
 
-
 curl -s -X POST -H "Content-Type: application/json" --data-binary @request.json  https://vision.googleapis.com/v1/images:annotate?key=${API_KEY} -o landmark-response.json
 
-gsutil cp landmark-response.json gs://$DEVSHELL_PROJECT_ID-bucket
+gsutil cp landmark-response.json gs://$PROJECT_ID-bucket
 
+echo "${BG_RED}${BOLD}Congratulations For Completing The Lab !!!${RESET}"
 
-
+#-----------------------------------------------------end----------------------------------------------------------#
